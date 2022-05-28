865371024b72c70930fbefe74b26a0762c79652a
SRD124 <santrdonoso@gmail.com>
2022-05-28 16:52:17 -0400

    Primer Bloque Cambiado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,7 +21,7 @@ const Home = () => {
         title={IntroContent.title}
         content={IntroContent.text}
         button={IntroContent.button}
-        icon="developer.svg"
+        icon="unknown.png"
         id="intro"
       />
       <MiddleBlock
